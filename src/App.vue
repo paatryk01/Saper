4fe5b1d58e72ec7bedd0084a39f4e0f807ecb208
paatryk01 <paatryk01@yahoo.pl>
2020-07-27 20:50:19 +0200

    tab size changes

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,54 @@
 <template>
-  <div id="app">
-    <Header 
-      :bombAmount='bombAmount'
-      :flags='flags'
-    />
-    <Board 
-      :bombAmount='bombAmount'
-      @flags='updateFlags'
-    />
-  </div>
+    <div id="app">
+        <Header 
+        :bombAmount='bombAmount'
+        :flags='flags'
+        />
+        <Board 
+        :bombAmount='bombAmount'
+        @flags='updateFlags'
+        />
+    </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Board from './components/Board.vue'
+    import Header from './components/Header.vue'
+    import Board from './components/Board.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Board
-  },
-  data() {
-    return {
-      bombAmount: 20,
-      flags: 0
-    }
-  },
-  methods: {
-    updateFlags(flags) {
-      this.flags = flags;
-    }
-  },
-  created() {
+    export default {
+        name: 'App',
+        components: {
+            Header,
+            Board
+        },
+        data() {
+            return {
+                bombAmount: 20,
+                flags: 0
+            }
+        },
+        methods: {
+            updateFlags(flags) {
+                this.flags = flags;
+            }
+        },
+        created() {
 
-  }
-}
+        }
+    }
 </script>
 
 <style>
 
-  @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
 
-  body {
+body {
     background: linear-gradient(160deg, #a6e0d9, #3dac9f);
     background-repeat: no-repeat;
     background-size: contain;
     height: 1200px;
     font-family: 'Righteous', cursive;
     text-transform: uppercase;
-  }
-  
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,28 @@
 <template>
-  <div>
-    <h1>MineSweeper</h1>
-    <h3>Marked fields: {{ flags }} 🚩 / {{ bombAmount }} 💣</h3>
-  </div>
+    <div>
+        <h1>MineSweeper</h1>
+        <h3>Marked fields: {{ flags }} 🚩 / {{ bombAmount }} 💣</h3>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Header',
-  props: {
-    bombAmount: Number,
-    flags: Number
-  }
-}
+    export default {
+    name: 'Header',
+    props: {
+        bombAmount: Number,
+        flags: Number
+        }
+    }
 </script>
 
 <style scoped>
-  
-  h1 {
-    font-size: 40px;
-  }
 
-  h1, h3{
+h1 {
+    font-size: 40px;
+}
+
+h1, h3{
     text-align: center;
-  }
+}
 
 </style>
